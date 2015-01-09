26fa9a2add358435addcc4cae0a1b49b6bd8ec0e
RyanTheAllmighty <rldowling03@gmail.com>
2015-01-09 15:34:08 +1100

    Change look and feel to that of the OS

--- src/main/java/me/ryandowling/twitchnotifier/App.java
+++ src/main/java/me/ryandowling/twitchnotifier/App.java
@@ -3,6 +3,8 @@ package me.ryandowling.twitchnotifier;
 import me.ryandowling.twitchnotifier.gui.MainFrame;
 
 import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
 
 public class App {
     public static final TwitchNotifier NOTIFIER = new TwitchNotifier();
@@ -12,6 +14,12 @@ public class App {
             @Override
             public void run() {
                 NOTIFIER.setup();
+                try {
+                    UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+                } catch (ClassNotFoundException | InstantiationException | IllegalAccessException |
+                        UnsupportedLookAndFeelException e) {
+                    e.printStackTrace();
+                }
                 new MainFrame();
             }
         });
