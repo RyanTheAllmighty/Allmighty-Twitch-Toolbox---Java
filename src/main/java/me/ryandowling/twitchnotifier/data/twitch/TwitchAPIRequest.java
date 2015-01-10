80036132afb03be9f9f19e0cc42a9d2ee9aec04e
RyanTheAllmighty <rldowling03@gmail.com>
2015-01-10 20:30:29 +1100

    Remove some duplicated code in the API Requests

--- src/main/java/me/ryandowling/twitchnotifier/data/twitch/TwitchAPIRequest.java
+++ src/main/java/me/ryandowling/twitchnotifier/data/twitch/TwitchAPIRequest.java
@@ -1,103 +1,16 @@
 package me.ryandowling.twitchnotifier.data.twitch;
 
-import me.ryandowling.twitchnotifier.Logger;
-import me.ryandowling.twitchnotifier.TwitchNotifier;
+import me.ryandowling.twitchnotifier.data.APIRequest;
 
-import javax.net.ssl.HttpsURLConnection;
-import java.io.BufferedReader;
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.URL;
-
-public class TwitchAPIRequest {
-    private static final String TWITCH_API_BASE = "https://api.twitch.tv/kraken";
-    private String path = "/";
-    private HttpsURLConnection connection;
-
+public class TwitchAPIRequest extends APIRequest {
     public TwitchAPIRequest(String path) {
-        if (path.length() == 0 || !path.substring(0, 1).equals("/")) {
-            this.path = "/" + path;
-        } else {
-            this.path = path;
-        }
+        super("https://api.twitch.tv/kraken", path);
     }
 
-    public String get() throws IOException {
-        this.connect("GET");
+    @Override
+    protected void setRequestProperties() {
+        super.setRequestProperties();
 
-        InputStream in = this.connection.getInputStream();
-        BufferedReader br = new BufferedReader(new InputStreamReader(in));
-        StringBuilder response = new StringBuilder();
-        String inputLine;
-
-        while ((inputLine = br.readLine()) != null) {
-            response.append(inputLine);
-        }
-
-        in.close();
-
-        this.disconnect();
-
-        return response.toString();
-    }
-
-    public String put(Object object) throws IOException {
-        this.connect("PUT", object);
-
-        InputStream in = this.connection.getInputStream();
-        BufferedReader br = new BufferedReader(new InputStreamReader(in));
-        StringBuilder response = new StringBuilder();
-        String inputLine;
-
-        while ((inputLine = br.readLine()) != null) {
-            response.append(inputLine);
-        }
-
-        in.close();
-
-        this.disconnect();
-
-        return response.toString();
-    }
-
-    private void connect(String requestMethod) throws IOException {
-        this.connect(requestMethod, null);
-    }
-
-    private void connect(String requestMethod, Object object) throws IOException {
-        Logger.log("Connecting to " + TWITCH_API_BASE + this.path);
-        URL url = new URL(TWITCH_API_BASE + this.path);
-        this.connection = (HttpsURLConnection) url.openConnection();
-        this.connection.setRequestMethod(requestMethod);
-        this.connection.setUseCaches(false);
-        this.connection.setDefaultUseCaches(false);
-        this.connection.setRequestProperty("Cache-Control", "no-store,max-age=0,no-cache");
-        this.connection.setRequestProperty("Expires", "0");
-        this.connection.setRequestProperty("Pragma", "no-cache");
         this.connection.setRequestProperty("Accept", "application/vnd.twitchtv.v3+json");
-        this.connection.setRequestProperty("Content-Type", "application/json");
-
-        if (object != null) {
-            this.connection.setRequestProperty("Content-Length", "" + TwitchNotifier.GSON.toJson(object).getBytes()
-                    .length);
-            connection.setDoOutput(true);
-        }
-
-        this.connection.connect();
-
-        if (object != null) {
-            DataOutputStream writer = new DataOutputStream(connection.getOutputStream());
-            writer.write(TwitchNotifier.GSON.toJson(object).getBytes());
-            writer.flush();
-            writer.close();
-        }
-    }
-
-    private void disconnect() {
-        if (this.connection != null) {
-            this.connection.disconnect();
-        }
     }
 }
